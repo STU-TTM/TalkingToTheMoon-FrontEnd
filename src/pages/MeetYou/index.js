3cddda31275ae2d8b47b4c17e5b099a9c8a40c1f
Bojiang2001 <99093915@qq.com>
2021-12-31 20:38:31 +0800

    新建卡片

--- src/pages/MeetYou/index.js
+++ src/pages/MeetYou/index.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../../component/Navbar";
 
-export default function index() {
-  return <div></div>;
+export default function Index() {
+  const [hoverOrNot, setHoverOrNot] = useState("block");
+  const [allList, setAllList] = useState({});
+  return (
+    <Navbar choice="Love">
+      <div className="w-full h-full flex justify-center items-center my-5 mx-5">
+        <div className="w-60 h-80 border-2 bg-white py-2 px-2 flex justify-center items-center relative">
+          {/* 展示照片位置 */}
+          <div
+            className="w-full h-full bg-gray-300"
+            onMouseOver={() => {
+              setHoverOrNot("block");
+            }}
+            // onMouseOut={() => {
+            //   setHoverOrNot("none");
+            // }}
+          ></div>
+          {/* hover显示位置 */}
+          <div
+            className="absolute bottom-0 w-full px-2 h-28 flex justify-around flex-col"
+            style={{
+              backgroundColor: "rgba(0,0,0,.3)",
+              display: `${hoverOrNot}`,
+            }}
+            onMouseOver={() => {
+              setHoverOrNot("block");
+            }}
+            // onMouseOut={() => {
+            //   setHoverOrNot("none");
+            // }}
+          >
+            {/* 点赞 */}
+            <div className="flex justify-between items-center text-xs">
+              <div></div>
+              <div className="">101k ❤</div>
+            </div>
+            {/* 头像以及昵称 */}
+            <div className="flex items-center">
+              {/* 头像 */}
+              <div className="w-9 h-9 rounded-full border-2 mr-2"></div>
+              {/* 昵称 */}
+              <div className="">昵称</div>
+            </div>
+            <div className="flex justify-center items-center h-12">
+              <div>text and description……………………</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Navbar>
+  );
 }
